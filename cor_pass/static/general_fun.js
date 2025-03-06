e9e4545774d4682aace32ef32ddb9fba266186f4
Chipizdry <chipizdry@gmail.com>
2025-03-06 18:48:35 +0200

    06.03.2025_Правки по стилям

--- cor_pass/static/general_fun.js
+++ cor_pass/static/general_fun.js
@@ -4,7 +4,7 @@ const modalConfigs = {
     corIdModal: { width: '300px', height: '500px', top: '300px', left: '250px' },
     editModal: { width: '250px', height: '500px', top: '50px', left: '50px' },
     myModal: { width: '250px', height: '450px', top: '50px', left: '250px' },
-    settingsModal: { width: '500px', height: '560px', top: '50px', left: '450px' },
+    settingsModal: { width: '550px', height: '560px', top: '50px', left: '450px' },
 };
 
 function makeModalDraggable(modalId) {
