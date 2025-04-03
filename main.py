2a0226ae33951afef443d95426519668dd3c5d58
Chipizdry <chipizdry@gmail.com>
2025-04-03 14:01:25 +0300

    03/04/2025 _Dicom_Viever

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ from starlette.responses import Response
 from cor_pass.routes import auth, person
 from cor_pass.database.db import get_db
 from cor_pass.database.redis_db import redis_client
+
+from cor_pass.dicom.router import router as dicom_router
+
 from cor_pass.routes import (
     auth,
     records,
@@ -22,6 +25,8 @@ from cor_pass.routes import (
     cor_id,
     otp_auth,
     admin,
+    lawyer,
+    doctor,
 )
 from cor_pass.config.config import settings
 from cor_pass.services.logger import logger
@@ -180,7 +185,9 @@ app.include_router(password_generator.router, prefix="/api")
 app.include_router(person.router, prefix="/api")
 app.include_router(cor_id.router, prefix="/api")
 app.include_router(otp_auth.router, prefix="/api")
-
+app.include_router(lawyer.router, prefix="/api")
+app.include_router(doctor.router, prefix="/api")
+app.include_router(dicom_router)
 
 if __name__ == "__main__":
     uvicorn.run(
