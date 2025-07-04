669847e776abd0f6b3ad3daabada5442d82da040
Chipizdry <chipizdry@gmail.com>
2025-07-04 13:50:29 +0300

    04.07.2025_Update

--- alembic/versions/631f7e68f91a_create_energy_storage_table.py
+++ alembic/versions/631f7e68f91a_create_energy_storage_table.py
@@ -1,8 +1,8 @@
-"""create cerbo_measurements table
+"""create energy storage table
 
-Revision ID: e435ef144668
+Revision ID: 631f7e68f91a
 Revises: 38b64534968d
-Create Date: 2025-07-04 12:34:45.345749
+Create Date: 2025-07-04 13:14:19.318597
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'e435ef144668'
+revision: str = '631f7e68f91a'
 down_revision: Union[str, None] = '38b64534968d'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
